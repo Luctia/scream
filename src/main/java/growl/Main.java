bceb4b22c1b2bd61da2327373830cd5fac2b724f
Luc Timmerman <48736891+Luctia@users.noreply.github.com>
2024-06-17 15:15:21 +0200

    K8s support (#3) (#12)
    * First draft of kubernetes files
    
    Functional, but the jmeter pod keeps restarting when exiting gracefully which is not desired.
    Also, no code is yet included of autogenerating these kubernetes files.
    
    * Change initContainers to containers
    
    * Renamed some values in k8s files
    
    * Code to generate k8s configurations
    
    * Add config.jmx for actions
    
    * Fix action
    
    * Fix action
    
    * Test action
    
    * Test action
    
    * Test action

--- src/main/java/growl/Main.java
+++ src/main/java/growl/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Configuration configuration = ConfigurationMaker.makeConfigurationFromFilename("config.json");
         XMLMaker.exportXML(configuration, true);
+        KubernetesMaker.generateK8sConfig(configuration);
     }
 }
